caf48cc9b73c04d58934ec7fc65cd9608dfad705
leejinwoo <leejinwoo1126@gmail.com>
2024-04-16 21:23:14 +0900

    feat: data.sql, 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO MEMBER (name) values ('tester1');
+INSERT INTO MEMBER (name) values ('tester2');
+INSERT INTO MEMBER (name) values ('tester3');
+INSERT INTO MEMBER (name) values ('tester4');
+INSERT INTO MEMBER (name) values ('tester5');
