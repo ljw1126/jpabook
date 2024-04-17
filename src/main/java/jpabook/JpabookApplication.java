dbe573b873a24ed81f55e0c7f9801f1500d87867
leejinwoo <leejinwoo1126@gmail.com>
2024-04-17 12:32:34 +0900

    feat: Hibernate5 모듈 빈 등록

--- src/main/java/jpabook/JpabookApplication.java
+++ src/main/java/jpabook/JpabookApplication.java
@@ -1,13 +1,22 @@
 package jpabook;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpabookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpabookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpabookApplication.class, args);
+    }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        // 강제 lazy loading 호출 활성화 -> default false 경우 null 로 필드값이 표시됨
+        //hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
