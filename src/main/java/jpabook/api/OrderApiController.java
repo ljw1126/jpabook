5cc7c2b04d7688abcc9ddcfb3c4c0f2cc30dd029
leejinwoo <leejinwoo1126@gmail.com>
2024-04-17 14:59:29 +0900

    feat: 페이징 한계 돌파, 컬렉션 조회 성능 최적화

--- src/main/java/jpabook/api/OrderApiController.java
+++ src/main/java/jpabook/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.repository.OrderSearch;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -56,6 +57,15 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream().map(OrderDto::new).collect(toList());
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3WithPaging(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         return orders.stream().map(OrderDto::new).collect(toList());
     }
 
